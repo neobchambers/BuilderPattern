78afc532845bcdebceb588a62bd73ef428aab1ed
neobchambers <tottalynotsatana@gmail.com>
2026-09-11 19:51:42 +0500

    I added GameCharacter product

---
+++ src/builder/GameCharacter.java
@@ -0,0 +1,30 @@
+package builder;
+
+public class GameCharacter {
+    private String name;
+    private String characterClass;
+    private String weapon;
+    private String armor;
+    private int level;
+    private String skills;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setCharacterClass(String characterClass){
+        this.characterClass = characterClass;
+    }
+    public void setWeapon(String weapon){
+        this.weapon = weapon;
+    }
+    public void setArmor(String armor){
+        this.armor = armor;
+    }
+    public void setLevel(int level){
+        this.level = level;
+    }
+    public void setSkills(String skills){
+        this.skills = skills;
+    }
+
+}
