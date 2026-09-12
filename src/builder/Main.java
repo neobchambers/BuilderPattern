098ca72729ef308ce614f8f53cb9f48f30ca9410
neobchambers <tottalynotsatana@gmail.com>
2026-09-13 00:29:12 +0500

    Added a space between characters

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -8,6 +8,7 @@ public class Main {
         GameCharacter dps = dpsdirector.buildCharacter("Steve", 10);
         System.out.println("DPS Character:");
         dps.showInfo();
+        System.out.println();
         //Healer build
         HealerBuilder healerBuilder = new HealerBuilder();
         CharacterDirector healerdirector = new CharacterDirector(healerBuilder);
@@ -15,5 +16,4 @@ public class Main {
         System.out.println("Healer Builder");
         healer.showInfo();
     }
-
 }
