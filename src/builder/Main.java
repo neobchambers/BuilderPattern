a74db27a7668bc82ddba59266f186403adeee237
neobchambers <tottalynotsatana@gmail.com>
2026-09-13 00:26:21 +0500

    Added showInfo method for dps and healer characters build

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -6,10 +6,14 @@ public class Main {
         DPSBuilder dpsBuilder = new DPSBuilder();
         CharacterDirector dpsdirector = new CharacterDirector(dpsBuilder);
         GameCharacter dps = dpsdirector.buildCharacter("Steve", 10);
-
+        System.out.println("DPS Character:");
+        dps.showInfo();
         //Healer build
         HealerBuilder healerBuilder = new HealerBuilder();
         CharacterDirector healerdirector = new CharacterDirector(healerBuilder);
         GameCharacter healer = healerdirector.buildCharacter("Leloush", 67);
+        System.out.println("Healer Builder");
+        healer.showInfo();
     }
+
 }
